e07db0530f5e3d21fae47fc94b7f56df559918b4
srujan ponnur <sponnur@ncsu.edu>
2021-10-25 16:52:37 -0400

    syntax fix

--- csc501-lab2/paging/release_bs.c
+++ csc501-lab2/paging/release_bs.c
@@ -7,7 +7,7 @@ SYSCALL release_bs(bsd_t bs_id) {
 
   /* release the backing store with ID bs_id */
    if (bs_id < 0 || bs_id > 7) {
-		return SYSERR
+	   return SYSERR;
    }
    bsm_tab[bs_id].bs_pid = BADPID;
    bsm_tab[bs_id].bs_status = BSM_UNMAPPED;
