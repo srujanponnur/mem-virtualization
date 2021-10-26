0a9ad20722991f0c692aaa28808f4a36f3bd569b
srujan ponnur <sponnur@ncsu.edu>
2021-10-25 23:54:51 -0400

    added proc 1 in main

--- csc501-lab2/sys/main.c
+++ csc501-lab2/sys/main.c
@@ -4,6 +4,42 @@
 #include <kernel.h>
 #include <proc.h>
 #include <stdio.h>
+#include <paging.h>
+
+
+#define PROC1_VADDR	0x40000000
+#define PROC1_VPNO      0x40000
+#define PROC2_VADDR     0x80000000
+#define PROC2_VPNO      0x80000
+#define TEST1_BS	1
+
+void proc1_test1(char* msg, int lck) {
+	char* addr;
+	int i,ret;
+
+	int ret = get_bs(TEST1_BS, 100);
+	kprintf("The return value is %d", ret);
+
+	/*if (xmmap(PROC1_VPNO, TEST1_BS, 100) == SYSERR) {
+		kprintf("xmmap call failed\n");
+		sleep(3);
+		return;
+	}
+
+	addr = (char*)PROC1_VADDR;
+	for (i = 0; i < 26; i++) {
+		*(addr + i * NBPG) = 'A' + i;
+	}
+
+	sleep(6);
+
+	for (i = 0; i < 26; i++) {
+		kprintf("0x%08x: %c\n", addr + i * NBPG, *(addr + i * NBPG));
+	}
+
+	xmunmap(PROC1_VPNO);*/
+	return;
+}
 
 /*------------------------------------------------------------------------
  *  main  --  user main program
@@ -16,5 +52,12 @@ int main()
         /* The hook to shutdown QEMU for process-like execution of XINU.
          * This API call terminates the QEMU process.
          */
-        shutdown();
+	int pid1;
+	int pid2;
+
+	kprintf("\n1: shared memory\n");
+	pid1 = create(proc1_test1, 2000, 20, "proc1_test1", 0, NULL);
+	resume(pid1);
+	sleep(10);
+    shutdown();
 }
